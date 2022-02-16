fa08645ee39bb901e0bac5a5c0265a2d9e407530
Edwin Kofler <edwin@kofler.dev>
2022-02-16 04:57:23 -0800

    feat: Write actual implementation

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A simple git-hooks solution
 
 STATUS: IN DEVELOPMENT
 
-Current solutions include [overcommit](https://github.com/sds/overcommit), [husky](https://github.com/typicode/husky), and [pre-commit](https://github.com/pre-commit/pre-commit). They are either too complex or subscribe to an ecosystem too much
+Current solutions include [overcommit](https://github.com/sds/overcommit), [husky](https://github.com/typicode/husky), and [pre-commit](https://github.com/pre-commit/pre-commit)
+
+Both overcommit and pre-commit are unecessarily complex
+
+Husky is a step in the right direction, but there are things I don't like. The wrapper script sources a `~/.huskyrc` and everything is written in NodeJS unnecessarily
 
 Hookah is a simple solution that makes it easy to ensure all developers of a particular project are using Git hooks
--- pkg/src/bin/hookah.sh
+++ pkg/src/bin/hookah.sh
@@ -1,8 +1,58 @@
 # shellcheck shell=bash
 
-main.hookah() {
-	git config core.hookDir './hooks'
+check() {
+	printf '%s\n' "$1"
+}
 
-	mkdir -p 
-	git config
+main.hookah() {
+	local subcmd="$1"
+
+	local hooksDir='./.hooks'
+
+	case $subcmd in
+	init)
+		git config --local core.hooksPath "$hooksDir"
+
+		mkdir -p "$hooksDir"
+		;;
+	check)
+		if [ ! -d "$hooksDir" ]; then
+			check "Hooks directory does not exist"
+		fi
+
+		local file=
+		for file in "$hooksDir"/*; do
+			if [ -r "$file" ]; then
+				check "File '$file' is not redable (does it have the correct permissions?). It will be skipped"
+			fi
+
+			if [ ! -x "$file" ]; then
+				check "File '$file' is not set as executable. It will be skipped"
+			fi
+
+			local -i i=0
+			while IFS= read -r line; do
+				if ((i == 0)); then
+					if [[ $line != \#!/* ]]; then
+						check "File '$file' does not have a proper shebang"
+					fi
+
+					local cmd="${line#\#!}"
+					cmd=${cmd%%* }
+					if [ -n "$cmd" ]; then
+						if ! command -v "$cmd" &>/dev/null; then
+							check "Shebang with command '$cmd' in '$file' does not actually exist"
+						fi
+					fi
+				fi
+
+				if ((i == 1)); then
+					break
+				fi
+
+				((++i))
+			done < "$file"; unset -v line
+		done; unset -v file
+		;;
+	esac
 }
